65ed646e5e5a77795785090519f932f8a035df6e
trandevin1 <trandevin2@gmail.com>
2024-09-30 16:43:36 -0700

    initial commit

---
+++ point.py
@@ -0,0 +1,14 @@
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Line:
+    def __init__(self, pt1, pt2):
+        self.pt1 = pt1
+        self.pt2 = pt2
+    
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.pt1.x, self.pt1.y, self.pt2.x, self.pt2.y, fill=fill_color, width=2)
