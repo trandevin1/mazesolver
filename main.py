4b5863ed8c3c524ae42c88a83f2abb889a4944a3
trandevin1 <trandevin2@gmail.com>
2024-10-01 15:58:36 -0700

    testing cell draw functionality

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
+
 from window import Window
+import point
+import cell
+
 
 def main():
-    win = Window(800,600)
+    win = Window(800, 600)
+
+    pt1 = point.Point(200, 500)
+    pt2 = point.Point(100, 200)
+    line = point.Line(pt1, pt2)
+
+    pt3 = point.Point(100, 100)
+    pt4 = point.Point(200, 200)
+    line2 = point.Line(pt3, pt4)
+
+    cell1 = cell.Cell(pt3, pt4)
+
+    win.draw_cell(cell1, "black")
+    # win.draw_line(line, "red")
+    # win.draw_line(line2, "black")
+
     win.wait_for_close()
 
 
