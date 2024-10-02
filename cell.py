e252f1d1f146c2a88129ee72f8e6fdf7e34981be
trandevin1 <trandevin2@gmail.com>
2024-10-02 15:55:07 -0700

    changed midpoint calc and added a way to draw broken walls

--- cell.py
+++ cell.py
@@ -2,7 +2,8 @@ from point import Line, Point
 
 
 class Cell:
-    def __init__(self, win):
+    def __init__(self, win=None):
+        self._visited = False
         self._win = win
         self._x1 = None
         self._y1 = None
@@ -18,14 +19,23 @@ class Cell:
         self._y1 = y1
         self._x2 = x2
         self._y2 = y2
+        white_top_line = Line(Point(x1, y1), Point(x2, y1))
+        white_bottom_line = Line(Point(x1, y2), Point(x2, y2))
+        white_left_line = Line(Point(x1, y1), Point(x1, y2))
+        white_right_line = Line(Point(x2, y1), Point(x2, y2))
+        self._win.draw_line(white_top_line, "white")
+        self._win.draw_line(white_bottom_line, "white")
+        self._win.draw_line(white_left_line, "white")
+        self._win.draw_line(white_right_line, "white")
+
         if self.has_top_wall:
             line = Line(Point(x1, y1), Point(x2, y1))
             self._win.draw_line(line)
-        
+
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
-        
+
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
             self._win.draw_line(line)
@@ -33,14 +43,20 @@ class Cell:
         if self.has_right_wall:
             line = Line(Point(x2, y1), Point(x2, y2))
             self._win.draw_line(line)
-        
+
     def draw_move(self, to_cell, undo=False):
         fill_color = "grey"
         if undo:
             fill_color = "red"
-        mid = ((self._x1 + self._x2) / 2, (self._y1 + self._y2) / 2)
-        mid_to_cell = ((to_cell._x1 + to_cell._x2) / 2, (to_cell._y1 + to_cell._y2) / 2)
+
+        mid = ((self._x1 + self._x2) // 2, (self._y1 + self._y2) // 2)
+        mid_to_cell = (
+            (to_cell._x1 + to_cell._x2) // 2,
+            (to_cell._y1 + to_cell._y2) // 2,
+        )
 
         line = Line(Point(mid[0], mid[1]), Point(mid_to_cell[0], mid_to_cell[1]))
         self._win.draw_line(line, fill_color)
 
+    def __repr__(self):
+        return f"Cell"
