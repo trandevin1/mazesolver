98dab3733e24eb23854779387232b01d5ddc8b49
trandevin1 <trandevin2@gmail.com>
2024-10-04 09:36:18 -0700

    added basic menubar

--- window.py
+++ window.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, BOTH, Canvas
+from tkinter import Tk, BOTH, Canvas, Menu
 
 
 class Window:
@@ -6,6 +6,8 @@ class Window:
         self.root_widget = Tk()
         self.root_widget.title("Main Window")
         self.root_widget.protocol("WM_DELETE_WINDOW", self.close)
+        self.root_widget.option_add("*tearoff", False)
+        self.init_menu_bar()
         self.canvas = Canvas(self.root_widget, bg="white", height=height, width=width)
         self.canvas.pack(fill=BOTH, expand=1)
         self.running = False
@@ -25,4 +27,9 @@ class Window:
     def draw_line(self, line, fill_color="black"):
         line.draw(self.canvas, fill_color)
 
-    
+    def init_menu_bar(self):
+        menubar = Menu(self.root_widget)
+        optionsmenu = Menu(menubar, tearoff=0)
+        optionsmenu.add_command(label="Exit", command=self.close)
+        menubar.add_cascade(label="Options", menu=optionsmenu)
+        self.root_widget.config(menu=menubar)
